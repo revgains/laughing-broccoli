b59c93a142326d27a3d29484947660a358731ad7
revgains <roger.rajala@gmail.com>
2019-01-13 22:52:00 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # didactic-parakeet
+#laughing-broccoli
---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-2',
+    BUCKET: 'scratch-serverless'
+  },
+  apiGateway: {
+    REGION: 'us-east-2',
+    URL: 'https://t2y3aelrqb.execute-api.us-east-2.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_roSxOWeBD',
+    APP_CLIENT_ID: '4itq2chvjipv83fb44p98tr96v',
+    IDENTITY_POOL_ID: 'us-east-2:3e8d6e30-e737-4512-9483-625757ae1ad1'
+  }
+};
